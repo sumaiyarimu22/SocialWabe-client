c1d955a98cc40324856807293c79b76d1e1bc2cd
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-06-21 16:04:00 +0600

    state manage uncompelete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scencs/homePage";
+import LoginPage from "scencs/loginPage";
+import ProfilePage from "scencs/profilePage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
