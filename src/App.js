333445c14ad4e9a1a63e15ed13f34572dfc8605f
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-06-22 23:59:19 +0600

    coustom theme & typography add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { BrowserRouter, /*Navigate,*/ Routes, Route } from "react-router-dom";
 import HomePage from "scencs/homePage";
 import LoginPage from "scencs/loginPage";
 import ProfilePage from "scencs/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
 const App = () => {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
